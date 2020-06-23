02009dcee600e2d08331ef08e1cf66af14a07f3b
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-06-22 21:52:53 -0400

    ci try 5

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -19,10 +19,6 @@ module.exports = {
             "error",
             4  
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
         "quotes": [
             "error",
             "single"
